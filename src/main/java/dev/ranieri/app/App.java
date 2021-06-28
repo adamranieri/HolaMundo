a6e926ae3e8f0ac8084e14f9fc35d6a10cf18707
adamranieri <adaman94@gmail.com>
2021-06-28 15:42:27 -0400

    hola mundo

---
+++ src/main/java/dev/ranieri/app/App.java
@@ -0,0 +1,17 @@
+package dev.ranieri.app;
+
+import io.javalin.Javalin;
+
+public class App {
+    static int counter = 0;
+
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> config.enableCorsForAllOrigins());
+
+        app.get("/hola",context -> {
+            context.result("Hola this many requests have been handled by the server " + ++App.counter);
+        });
+
+        app.start();
+    }
+}
